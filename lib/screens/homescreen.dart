01d959c33b5a59bb71fa88250024058c2166392e
fuadagus <fagussalim518@gmail.com>
2021-12-18 05:12:13 +0700

    add homescreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -7,13 +7,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _restaurantList.length,
-      itemBuilder: (context, index) {
-        return Container(
-          child: Text("test"),
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: ListView.builder(
+            itemCount: _restaurantList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: Image.network(
+                            _restaurantList[index]["pictureId"],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Expanded(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 50,
+                              child: Expanded(
+                                child: Text(
+                                  _restaurantList[index]["name"],
+                                  textAlign: TextAlign.start,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              _restaurantList[index]["city"],
+                              textAlign: TextAlign.start,
+                            )
+                          ],
+                        ))
+                      ],
+                    )),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
